aab0de389cb02559f88f401da8c8f3f045f22c0f
BricePrs <brice.peres@gmail.com>
2022-11-03 23:49:13 +0100

    nice project

--- site/Projects/scripts/project_focus.js
+++ site/Projects/scripts/project_focus.js
@@ -1,15 +1,19 @@
 
 
 const projects = document.getElementsByClassName("project");
+const main = document.getElementsByTagName("main")[0];
+const header = document.getElementById("top-bar");
 
 function onClickEvent(event) {
     let project = event.target.closest(".project");
     let state = project.getAttribute("state");
-    for (let i = 0; i < projects.length; i++) {
-        projects[i].setAttribute("state", "unfocused")
-    }
+    unfocus_all();
     if (state === "unfocused") {
         project.setAttribute("state", "focused")
+        window.scrollBy({
+            top: +project.getBoundingClientRect().top - header.offsetHeight*1.2,
+            behavior: 'smooth'
+        });
     }
 }
 
@@ -19,6 +23,17 @@ for (let i = 0; i < projects.length; i++) {
     projects[i].addEventListener("click", onClickEvent);
 }
 
+if(document.addEventListener){
+      document.addEventListener('wheel',mouseScroll,false);
+ }
 
 
+function mouseScroll(e) {
+    unfocus_all();
+}
 
+function unfocus_all() {
+    for (let i = 0; i < projects.length; i++) {
+        projects[i].setAttribute("state", "unfocused")
+    }
+}
